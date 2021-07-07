792e27ed12c69b6d5d5265b727980c5762a3dcbb
NXSINO <39287688+NXSINO@users.noreply.github.com>
2021-07-07 15:55:11 +0800

    update

--- layuiadmin/config.js
+++ layuiadmin/config.js
@@ -1,12 +1,3 @@
-/**
-
- @Name：layuiAdmin iframe版全局配置
- @Author：贤心
- @Site：http://www.layui.com/admin/
- @License：LPPL（layui付费产品协议）
-    
- */
- 
 layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
   exports('setter', {
     container: 'LAY_app' //容器ID
@@ -17,7 +8,7 @@ layui.define(['laytpl', 'layer', 'element', 'util'], function(exports){
     ,pageTabs: true //是否开启页面选项卡功能。iframe版推荐开启
     
     ,name: 'layuiAdmin'
-    ,tableName: 'layuiAdmin' //本地存储表名
+    ,tableName: 'NxsinoAdmin' //本地存储表名
     ,MOD_NAME: 'admin' //模块事件名
     
     ,debug: true //是否开启调试模式。如开启，接口异常时会抛出异常 URL 等信息
